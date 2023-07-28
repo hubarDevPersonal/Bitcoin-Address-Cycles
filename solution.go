9fb35f3fb808447f76e88ee43361683d8212f099
Artem Gubar <artemgubar@Artems-MacBook-Pro.local>
2023-07-28 14:01:37 +0200

    Second iteration

--- solution.go
+++ solution.go
@@ -47,6 +47,7 @@ func (s *SolutionServer) MineBlock(ctx context.Context, request *rpc.MineBlockRe
 // ToBlock - block to end the search at (inclusive).
 // Address cycle is a path in the graph from one vertex to the other with the same address.
 func (s *SolutionServer) CountCycles(ctx context.Context, request *rpc.CountCyclesRequest) (*rpc.CountCyclesResponse, error) {
+	visited := make(map[string]bool)
 	var numberOfCycles int64 = 0
 
 	// Iterate through all blocks in the specified range
@@ -58,7 +59,7 @@ func (s *SolutionServer) CountCycles(ctx context.Context, request *rpc.CountCycl
 				// Iterate through all vertices in the transaction's outputs
 				for _, output := range transaction.Outputs {
 					// Perform DFS to find cycles starting from the current output vertex
-					numberOfCycles += s.findCycleDFS(output, request.MaxCycleLength, make(map[string]bool), 1)
+					numberOfCycles += s.findCycleDFS(output, output, request.MaxCycleLength, visited, 1)
 				}
 			}
 		}
@@ -69,13 +70,15 @@ func (s *SolutionServer) CountCycles(ctx context.Context, request *rpc.CountCycl
 
 // findCycleDFS is a helper function that performs Depth-First Search (DFS)
 // to find cycles starting from the given vertex.
-func (s *SolutionServer) findCycleDFS(currentVertex *rpc.Vertex, maxCycleLength int64, path map[string]bool, currentCycleLength int64) int64 {
-	if currentCycleLength > maxCycleLength {
+func (s *SolutionServer) findCycleDFS(startVertex *rpc.Vertex, currentVertex *rpc.Vertex, maxCycleLength int64, visited map[string]bool, currentCycleLength int64) int64 {
+	// Check if the current vertex has already been visited
+	vertexKey := fmt.Sprintf("%s-%d", currentVertex.TxHash, currentVertex.Index)
+	if visited[vertexKey] {
 		return 0
 	}
 
-	// Mark the current vertex address as visited in the current path
-	path[currentVertex.Address] = true
+	// Mark the current vertex as visited
+	visited[vertexKey] = true
 
 	// Initialize the count of cycles found from this vertex
 	cycleCount := int64(0)
@@ -85,18 +88,19 @@ func (s *SolutionServer) findCycleDFS(currentVertex *rpc.Vertex, maxCycleLength 
 		for _, transaction := range block.Transactions {
 			// Check if the transaction has the currentVertex as an input
 			for _, input := range transaction.Inputs {
-				if input.TxHash == currentVertex.TxHash && input.Index == currentVertex.Index {
+				if input.TxHash == currentVertex.TxHash {
 					// The transaction has the currentVertex as an input.
 					// Now, search for vertices in the transaction's outputs with the same address.
 					for _, output := range transaction.Outputs {
-						if output.Address == currentVertex.Address {
+						if output.Address == startVertex.Address {
 							// Found a cycle! Increment the cycle count
+							fmt.Println(output.Address, startVertex.Address, output.TxHash, startVertex.TxHash)
 							cycleCount++
 						}
 
 						// Continue DFS to explore the adjacent vertex
-						if !path[output.Address] {
-							cycleCount += s.findCycleDFS(output, maxCycleLength, path, currentCycleLength+1)
+						if currentCycleLength+1 <= maxCycleLength {
+							cycleCount += s.findCycleDFS(startVertex, output, maxCycleLength, visited, currentCycleLength+1)
 						}
 					}
 				}
@@ -104,8 +108,8 @@ func (s *SolutionServer) findCycleDFS(currentVertex *rpc.Vertex, maxCycleLength 
 		}
 	}
 
-	// Mark the current vertex address as unvisited before returning from the function
-	path[currentVertex.Address] = false
+	// Mark the current vertex as unvisited before returning from the function
+	visited[vertexKey] = false
 
 	return cycleCount
 }
